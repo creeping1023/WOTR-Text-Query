62dab481183545ce304f07ce8bc4e814e5d5077a
creeping1023 <creeping1023@gmail.com>
2024-07-26 09:55:38 +0800

    refactor: move index to Main

---
+++ src/Main.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import Home from './Home';
+import Results from './Results';
+import Entity from './Entity';
+import String from './String';
+import SearchOnTop from './SearchOnTop'
+import { Link, Route, Routes } from 'react-router-dom';
+import { FloatButton, Layout, Button, Tooltip } from "antd";
+import { ArrowUpOutlined, GithubOutlined, HomeOutlined } from '@ant-design/icons';
+
+export default function Main(){
+  return (
+    <Layout style={{ height: '100vh' }}>
+      <Layout.Header style={{display:'flex', alignItems:'center', gap:'10px', boxShadow:'0 1px 2px 0 rgba(128, 128, 128, 0.06),0 1px 6px -1px rgba(128, 128, 128, 0.04),0 2px 4px 0 rgba(128, 128, 128, 0.04)'}}>
+        <Tooltip placement="bottom" title="Home">
+          <Link to={'/'} className='ant-btn ant-btn-circle ant-btn-default ant-btn-icon-only'><HomeOutlined /></Link>
+        </Tooltip>
+        <div style={{flexGrow:1, display: 'flex'}} >
+          <SearchOnTop style={{maxWidth:'500px'}} />
+        </div>
+        <Tooltip placement="bottom" title="Github">
+          <Button shape="circle" icon={<GithubOutlined />} href="https://github.com/creeping1023/WOTR-Text-Query" target='_blank'/>
+        </Tooltip>
+      </Layout.Header>
+      <Layout.Content style={{ overflowY: 'scroll',  overflowX: 'hidden' }} id="main-content">
+        <Routes>
+          <Route path='/' element={<Home/>}></Route>
+          <Route path='/results' element={<Results/>}></Route>
+          <Route path='/entity/:id' element={<Entity/>}></Route>
+          <Route path='/string/:id' element={<String/>}></Route>
+        </Routes>
+        <FloatButton.BackTop target={()=>document.getElementById("main-content")!} visibilityHeight={window.innerHeight/2} duration={450} icon={<ArrowUpOutlined />} />
+      </Layout.Content>
+    </Layout>
+  );
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Home from './Home';
-import Results from './Results';
-import Entity from './Entity';
-import String from './String';
-import SearchOnTop from './SearchOnTop'
 import { SharedStateProvider } from "./SharedStateContext";
-import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
 import { HelmetProvider } from "react-helmet-async";
-import { FloatButton, Layout, Button, Tooltip, ConfigProvider, theme, ThemeConfig } from "antd";
-import { ArrowUpOutlined, GithubOutlined, HomeOutlined } from '@ant-design/icons';
+import { ConfigProvider, ThemeConfig } from "antd";
+import Main from './Main';
 
 const themeConfig:ThemeConfig = {
   hashed: false,
@@ -32,35 +27,11 @@ const themeConfig:ThemeConfig = {
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
-let main = (
-  <ConfigProvider theme={themeConfig}>
-    <Layout style={{ height: '100vh' }}>
-      <Layout.Header style={{display:'flex', alignItems:'center', gap:'10px', boxShadow:'0 1px 2px 0 rgba(128, 128, 128, 0.06),0 1px 6px -1px rgba(128, 128, 128, 0.04),0 2px 4px 0 rgba(128, 128, 128, 0.04)'}}>
-        <Tooltip placement="bottom" title="Home">
-          <Link to={'/'} className='ant-btn ant-btn-circle ant-btn-default ant-btn-icon-only'><HomeOutlined /></Link>
-        </Tooltip>
-        <div style={{flexGrow:1, display: 'flex'}} >
-          <SearchOnTop style={{maxWidth:'500px'}} />
-        </div>
-        <Tooltip placement="bottom" title="Github">
-          <Button shape="circle" icon={<GithubOutlined />} href="https://github.com/creeping1023/WOTR-Text-Query" target='_blank'/>
-        </Tooltip>
-      </Layout.Header>
-      <Layout.Content style={{ overflowY: 'scroll',  overflowX: 'hidden' }} id="main-content">
-        <Routes>
-          <Route path='/' element={<Home/>}></Route>
-          <Route path='/results' element={<Results/>}></Route>
-          <Route path='/entity/:id' element={<Entity/>}></Route>
-          <Route path='/string/:id' element={<String/>}></Route>
-        </Routes>
-        <FloatButton.BackTop target={()=>document.getElementById("main-content")!} visibilityHeight={window.innerHeight/2} duration={450} icon={<ArrowUpOutlined />} />
-      </Layout.Content>
-    </Layout>
-  </ConfigProvider>
-);
+let main = <Main />
+main = <ConfigProvider theme={themeConfig}>{main}</ConfigProvider>
 main = <SharedStateProvider>{main}</SharedStateProvider>;
 main = <HelmetProvider>{main}</HelmetProvider>;
-main = <Router basename={process.env.PUBLIC_URL}>{main}</Router>;
+main = <BrowserRouter basename={process.env.PUBLIC_URL}>{main}</BrowserRouter>;
 main = <React.StrictMode>{main}</React.StrictMode>;
 root.render(main);
 
